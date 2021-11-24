26bc2f6da6e7c1cccf39450e45fa6b41755c5578
kicokang <88639914+kicokang@users.noreply.github.com>
2021-11-24 15:21:21 +0900

    navermap

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    naverMap();
+});
+
+function naverMap(){
+
+    var mapOptions = {
+        center: new naver.maps.LatLng(37.3595704, 127.105399),
+        zoom: 10
+    };
+    
+    var map = new naver.maps.Map('map', mapOptions);
+
+}
